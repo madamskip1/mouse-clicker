3560c987b08f943c02acc43688b91b9d1e93dfe9
Maciej Adamski <madamskip1@gmail.com>
2026-09-11 22:45:13 +0200

    Move cursor on linux

--- mouse-clicker/src/mouse-linux.cpp
+++ mouse-clicker/src/mouse-linux.cpp
@@ -1,24 +1,53 @@
 #include "mouse-button.hpp"
 #include "mouse.hpp"
 
+#include <X11/Xlib.h>
+#include <cassert>
 #include <memory>
 #include <print>
 
 namespace mouse_clicker
 {
 
-
 class MouseLinux final : public Mouse
 {
 public:
-    MouseLinux() = default;
+    MouseLinux();
+    ~MouseLinux();
+
+    MouseLinux(const MouseLinux&) = delete;
+    auto operator=(const MouseLinux&) -> MouseLinux& = delete;
+
+    MouseLinux(MouseLinux&&) = delete;
+    auto operator=(MouseLinux&&) -> MouseLinux& = delete;
 
     auto click(Button button, unsigned int x, unsigned int y) -> void final;
+
+private:
+    Display* display{ nullptr };
+
+    auto moveCursor(unsigned int x, unsigned int y) -> void;
 };
+
+MouseLinux::MouseLinux()
+    : display(XOpenDisplay(nullptr))
+{
+    assert(display != nullptr);
+}
+
+MouseLinux::~MouseLinux()
+{
+    if (display != nullptr)
+    {
+        XCloseDisplay(display);
+    }
+}
 
 
 auto MouseLinux::click(Button button, unsigned int x, unsigned int y) -> void
 {
+    moveCursor(x, y);
+
     auto mouseButtonToString = [](Button button) -> std::string {
         using enum mouse_clicker::Button;
 
@@ -36,6 +65,14 @@ auto MouseLinux::click(Button button, unsigned int x, unsigned int y) -> void
     };
 
     std::println("Clicking {} at ({}, {})", mouseButtonToString(button), x, y);
+}
+
+auto MouseLinux::moveCursor(unsigned int x, unsigned int y) -> void
+{
+    const auto rootWindow = DefaultRootWindow(display);
+
+    XWarpPointer(display, None, rootWindow, 0, 0, 0, 0, static_cast<int>(x), static_cast<int>(y));
+    XFlush(display);
 }
 
 auto createMouse() -> std::unique_ptr<Mouse>
